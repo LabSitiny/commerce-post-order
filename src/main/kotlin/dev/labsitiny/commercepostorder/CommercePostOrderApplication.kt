ae08b2fb29fcc62a9c0ec54acb08c449b9137e43
Parkseongheum <ab6449@gmail.com>
2022-11-27 19:38:41 +0900

    Init: Project Post order service

---
+++ src/main/kotlin/dev/labsitiny/commercepostorder/CommercePostOrderApplication.kt
@@ -0,0 +1,11 @@
+package dev.labsitiny.commercepostorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommercePostOrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommercePostOrderApplication>(*args)
+}
---
+++ src/test/kotlin/dev/labsitiny/commercepostorder/CommercePostOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.labsitiny.commercepostorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommercePostOrderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
